cc569a68b394742186a57adc1dad4f8cb7fde1a7
Gijs Nijholt <gijs.nijholt@gmail.com>
2017-03-09 11:38:45 +0100

    fix(Dev): Adds proxying to real back-end to server.js for development mode.

--- server.js
+++ server.js
@@ -1,26 +1,42 @@
-var webpack = require('webpack');
-var webpackDevMiddleware = require('webpack-dev-middleware');
-var webpackHotMiddleware = require('webpack-hot-middleware');
-var config = require('./webpack.config');
-var express = require('express');
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+const config = require('./webpack.config');
+const express = require('express');
+const request = require('request');
 
-var app = new (express)();
-var port = 3000;
+const app = new (express)();
+const port = 5000;
 
-var compiler = webpack(config);
+const compiler = webpack(config);
 app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
 app.use(webpackHotMiddleware(compiler));
 
-app.use(express.static(__dirname + '/'));  
+app.use(express.static(__dirname + '/'));
 
-app.get("/", function(req, res) {
-  res.sendFile(__dirname + '/index.hmr.html');
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
 });
 
-app.listen(port, function(error) {
+app.use('/api', (req, res) => {
+  const url = 'https://waterlabel.sandbox.lizard.net/api' + req.url;
+  const headers = {
+    'username': process.env.sso_user,
+    'password': process.env.sso_pass,
+  };
+  req.pipe(request({
+    url,
+    headers,
+  })).pipe(res);
+});
+
+app.listen(port, (error) => {
   if (error) {
     console.error(error);
-  } else {
-    console.info("==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port);
+  }
+  else {
+    console.info('==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.', port, port);
   }
 });
