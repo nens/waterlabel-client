69c4c80182abbec58b19578d59e95432c728b2f2
Tom de Boer <tom.deboer@nelen-schuurmans.nl>
2018-12-07 12:15:36 +0100

    update webpack max image size

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -54,7 +54,7 @@ var config = {
         loader: 'transform/cacheable?brfs',
       },
       { test: /\.(png|jpg|svg|woff|eot|ttf|otf)$/,
-        loader: 'url-loader?limit=100000',
+        loader: 'url-loader?limit=100000000',
       },
     ],
   },
